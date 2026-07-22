30a23d53674f46803db4e22a5a07181644c902f7
Sarika Pandey <sarikapandey686@gmail.com>
2026-07-22 18:14:09 +0000

    fastly

--- Utils/prepocess.py
+++ Utils/prepocess.py
@@ -1,10 +1,20 @@
-import streamlit as st 
-uploaded_file = st.file_uploader("Upload Gate Pass Log", type=["xlsx"])
-from utils.preprocess import load_data
 
-df = load_data(uploaded_file)
 import pandas as pd
+
 
 def load_data(uploaded_file):
     df = pd.read_excel(uploaded_file)
+    return df
+
+
+def preprocess(df):
+
+    # Convert time columns
+    df["entry_time"] = pd.to_datetime(df["entry_time"])
+    df["exit_time"] = pd.to_datetime(df["exit_time"])
+
+    # Create new features
+    df["entry_hour"] = df["entry_time"].dt.hour
+    df["exit_hour"] = df["exit_time"].dt.hour
+
     return df
