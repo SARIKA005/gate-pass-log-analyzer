1d2fde07b591d5bc772451efd5118bc178e2efbd
Meemansha2005 <pandeymeemansha617@gmail.com>
2026-07-26 10:44:15 +0000

    Fixed Excel data type issue and added upload chart

--- Frontend/views/upload.py
+++ Frontend/views/upload.py
@@ -15,34 +15,58 @@ def show_upload():
             del st.session_state["data"]
             st.rerun()
 
+        df = st.session_state["data"]
+
         st.subheader("Preview")
         st.dataframe(
-            st.session_state["data"],
+            df,
             use_container_width=True
         )
 
+        # Gate-wise Bar Chart
+        if "Gate No" in df.columns:
+            st.subheader("🚪 Gate-wise Visitor Analysis")
+
+            gate_count = df["Gate No"].value_counts()
+
+            st.bar_chart(gate_count)
+
         return
+
 
     uploaded_file = st.file_uploader(
         "Choose an Excel file",
         type=["xlsx"]
     )
 
+
     if uploaded_file is not None:
 
         try:
 
             df = pd.read_excel(uploaded_file)
-
+            df = df.astype(str)   
             st.session_state["data"] = df
 
             st.success("✅ File uploaded successfully!")
 
             st.subheader("Preview")
+
             st.dataframe(
                 df,
                 use_container_width=True
             )
+
+
+            # Gate-wise Bar Chart
+            if "Gate No" in df.columns:
+
+                st.subheader("🚪 Gate-wise Visitor Analysis")
+
+                gate_count = df["Gate No"].value_counts()
+
+                st.bar_chart(gate_count)
+
 
         except Exception as e:
 
